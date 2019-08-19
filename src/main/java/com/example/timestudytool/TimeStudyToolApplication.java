0c70ead84273cc1ad9e05b1eeae034b7bc1bc559
Isaac Arcos <isaac.arcos696@gmail.com>
2019-08-19 12:33:07 -0500

    initial commit

---
+++ src/main/java/com/example/timestudytool/TimeStudyToolApplication.java
@@ -0,0 +1,14 @@
+package com.example.timestudytool;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeStudyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeStudyToolApplication.class, args);
+	}
+
+}
